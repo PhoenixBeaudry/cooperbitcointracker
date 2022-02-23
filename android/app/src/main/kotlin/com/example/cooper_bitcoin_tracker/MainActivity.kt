d9b3eb6193396f6196f9ffbfa4d49160bd233e4c
Phoenix Beaudry <phoenixdbeaudry@gmail.com>
2022-02-23 14:42:56 -0800

    Setup CooperBitcoinTracker, queries coingecko api, displays btc price on screen

---
+++ android/app/src/main/kotlin/com/example/cooper_bitcoin_tracker/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.cooper_bitcoin_tracker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'CooperBitcoinTracker',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const BitcoinTracker(),
+    );
+  }
+}
+
+Future<int> fetchBitcoinPrice() async {
+  final response = await http.get(Uri.parse(
+      'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=CAD'));
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    return jsonDecode(response.body)["bitcoin"]["cad"];
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to fetch BTC price');
+  }
+}
+
+class BitcoinTracker extends StatefulWidget {
+  const BitcoinTracker({Key? key}) : super(key: key);
+
+  @override
+  _BitcoinTrackerState createState() => _BitcoinTrackerState();
+}
+
+class _BitcoinTrackerState extends State<BitcoinTracker> {
+  late Future<int> bitcoinPrice;
+  @override
+  void initState() {
+    super.initState();
+    bitcoinPrice = fetchBitcoinPrice();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Cooper Bitcoin Tracker',
+      theme: ThemeData(
+        primarySwatch: Colors.grey,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Cooper Bitcoin Tracker'),
+        ),
+        body: Center(
+          child: Container(
+            padding: const EdgeInsets.all(15.0),
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.black,
+                width: 4,
+              ),
+              borderRadius: BorderRadius.circular(5),
+            ),
+            child: FutureBuilder<int>(
+                future: bitcoinPrice,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    int currentBitcoinPrice =
+                        int.parse(snapshot.data.toString());
+                    return Text(currentBitcoinPrice.toString());
+                  } else if (snapshot.hasError) {
+                    return const Text('Error getting bitcoin price');
+                  }
+                  return const CircularProgressIndicator();
+                }),
+          ),
+        ),
+      ),
+    );
+  }
+}
